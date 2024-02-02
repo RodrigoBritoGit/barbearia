79c617ee89b98c0153acd3ab8c5ec70601910f26
RodrigoBritoGit <Rodrigodossantosbrito@hotmail.com>
2024-02-01 23:11:10 -0300

    criando booking-item tela inicial

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,32 @@
 import { format } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
+import BookingItem from "../_components/booking-item";
 
 export default function Home() {
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="px-5 pt-5">
-      <h2 className="text-xl font-bold">Olá, Rodrigo</h2>
-      <p className="text-sm">
-        {format(new Date(), "EEEE',' dd 'de' MMMM 'de' YYY",{
+        <h2 className="text-xl font-bold">Olá, Rodrigo</h2>
+        <p className="text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM 'de' YYY", {
             locale: ptBR,
-      }
-      )}</p>
+          }
+          )}</p>
       </div>
+
+      <div className="px-5 mt-8">
+        <Search />
+      </div>
+
+      <div className=" px-5 mt-6">
+        <h2 className="text-sm mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+        <BookingItem/>
+      </div>
+
+
     </div>
   );
 }
