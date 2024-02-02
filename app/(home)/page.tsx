6f5b7471cde985d578ac840fdd0382a6d06227e8
RodrigoBritoGit <Rodrigodossantosbrito@hotmail.com>
2024-02-01 21:39:54 -0300

    Mensagem de boas vindas pagina inicial

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,19 @@
-import Image from "next/image";
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <div className="px-5 pt-5">
+      <h2 className="text-xl font-bold">Olá, Rodrigo</h2>
+      <p className="text-sm">
+        {format(new Date(), "EEEE',' dd 'de' MMMM 'de' YYY",{
+            locale: ptBR,
+      }
+      )}</p>
+      </div>
     </div>
   );
 }
