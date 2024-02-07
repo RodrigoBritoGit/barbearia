7c10f863a87fda7b19314aad55a5fdfc5c311b00
RodrigoBritoGit <Rodrigodossantosbrito@hotmail.com>
2024-02-06 22:35:24 -0300

    Atualizando pagina Home

---
+++ app/(home)/_components/barbershop-item.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/app/_components/ui/button";
+import { Card, CardContent } from "@/app/_components/ui/card";
+import { Barbershop } from "@prisma/client";
+import Image from "next/image";
+
+interface BarbershopItemProps {
+    barbershop: Barbershop;
+}
+
+const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+    return (
+        <Card className="min-w-[250px] max-w-[250px] rounded-2xl">
+            <CardContent className="p-2">
+                <Image alt={barbershop.name} src={barbershop.imageUrl} height={0} width={0} sizes="100vw" className="h-[159px] w-full rounded-2xl" />
+                <div className="px-3 pb-3">
+                    <h2 className=" mt-2 font-bold overflow-hidden text-allipsis text-nowrap">{barbershop.name}</h2>
+                    <p className=" mt-2 text-sm text-gray-400 overflow-hidden text-allipsis text-nowrap">{barbershop.address}</p>
+                    <Button className="w-full mt-3" variant={"secondary"}>Reservar</Button>
+                </div>
+
+            </CardContent>
+        </Card>
+    );
+}
+
+export default BarbershopItem;
---
+++ app/_components/footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return ( 
+        <div className="w-full bg-secondary py-5 px-5">
+            <p className="text-gray-400 text-xs font-bold opacity-75"> ₢ 2024 Copywright FSW Barber </p>
+        </div>
+        
+     );
+}
+ 
+export default Footer;
